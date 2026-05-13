46d2a57142926f03a4768c7e0788bdc50d281322
Paul Terry <paul.terry84@gmail.com>
2026-05-13 11:18:00 +0100

    moved application startup code to main.py and api/health_check is now api/health

--- merger-api/app/health_check/routes.py
+++ merger-api/app/health_check/routes.py
@@ -4,8 +4,8 @@ from fastapi import APIRouter
 
 from app.health_check.schemas import HealthCheckResponse
 
-router = APIRouter()
+router = APIRouter(prefix="/health")
 
 @router.get("/", response_model=HealthCheckResponse, status_code=HTTPStatus.OK.value)
 async def health_check() -> HealthCheckResponse:
-    return {"healthy": "Yes!"}
+    return HealthCheckResponse(healthy="Yes!")
--- merger-api/app/router.py
+++ merger-api/app/router.py
@@ -3,4 +3,4 @@ from fastapi import APIRouter
 from app.health_check.routes import router as health_check_router
 
 api_router = APIRouter(prefix="/api")
-api_router.include_router(router=health_check_router, tags=["health check"], prefix="/health_check")
+api_router.include_router(router=health_check_router, tags=["health check"])
